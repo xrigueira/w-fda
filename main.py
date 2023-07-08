1ac875c4253cec3423e7e4ba249b618dbcfdb2b5
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-07-08 12:35:14 -0700

    Added the plots

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Try to run get_msa in this Python file and print its results
 # or put them in a Pandas database
 
+import csv
 import numpy as np
 import rpy2.robjects as robjects
+import plotly.graph_objects as go
 
 from outDec import outDec
+from datetime import datetime
 
 # # Load the R function from msaCalc.R
 # with open('msaCalc.R', 'r') as file:
@@ -25,5 +28,95 @@ shape = msa[:, 1]
 amplitude = msa[:, 2]
 
 outliers_in_data = outDec(magnitude=magnitude, shape=shape, amplitude=amplitude)
+
+# Get time stamps
+#########Turn this into a function#########
+
+# Define file path
+data_file = 'data/labeled_901_pro.csv'
+
+# Open the CSV file and read the data
+with open(data_file, 'r') as file:
+    csv_reader = csv.DictReader(file)
+    
+    # Initialize a list to store unique datetime objects
+    unique_days = []
+    
+    # Iterate over each row in the dataset
+    for row in csv_reader:
+        # Extract the date and convert it to a datetime object
+        date_str = row['date']
+        datetime_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+        
+        # Truncate the time information (optional)
+        datetime_obj = datetime_obj.replace(hour=0, minute=0, second=0, microsecond=0)
+        
+        # Add the datetime object to the unique_days list if it's not already present
+        if datetime_obj not in unique_days:
+            unique_days.append(datetime_obj)
+        
+# Convert unique_days to a numpy array and change datetime format
+unique_days = [datetime_obj.strftime('%d-%m-%Y') for datetime_obj in unique_days]
+timestamps = np.array(unique_days)
+##################
+
+# Now I have to build the outlier detector (unsupervised kNN)
+contamination = 0.1
+from sklearn.neighbors import NearestNeighbors
+
+modelkNN = NearestNeighbors(n_neighbors=10, algorithm='ball_tree')
+modelkNN.fit(msa)
+distances, indexes = modelkNN.kneighbors(msa)
+
+indexkNN = np.where(distances.mean(axis=1) >= np.quantile(distances.mean(axis=1), (1-contamination)))
+valueskNN = msa[indexkNN]
+timestampskNN = timestamps[indexkNN]
+
+# # Plot distance
+# fig = go.Figure(data=[go.Scatter3d(x=magnitude, y=shape, z=amplitude, mode='markers', 
+#                                 marker=dict(color=distances.mean(axis=1), colorscale='Viridis', colorbar=dict(title='Distance')),
+#                                 hovertemplate='<b>Timestamp:</b> %{text}<br>'
+#                                 '<b>Magnitude:</b> %{x}<br>'
+#                                 '<b>Shape:</b> %{y}<br>'
+#                                 '<b>Amplitude:</b> %{z}<br>'
+#                                 '<b>Distance:</b> %{marker.color}<extra></extra>',
+#                                 text=unique_days)])
+
+# # Set layout options
+# fig.update_layout(
+#     scene=dict(
+#         xaxis=dict(title='Magnitude'),
+#         yaxis=dict(title='Shape'),
+#         zaxis=dict(title='Amplitude')
+#     ),
+#     title='Distance MSA Plot'
+# )
+
+# # Show the plot
+# fig.show()
+
+# Plot outliers
+fig = go.Figure(data=[go.Scatter3d(x=magnitude, y=shape, z=amplitude, mode='markers', 
+                                marker=dict(color=np.where(np.isin(np.arange(len(magnitude)), indexkNN), 'red', 'blue'), colorscale=[[0, 'blue'], [1, 'red']], colorbar=dict(title='Outlier')),
+                                hovertemplate='<b>Timestamp:</b> %{text}<br>'
+                                '<b>Magnitude:</b> %{x}<br>'
+                                '<b>Shape:</b> %{y}<br>'
+                                '<b>Amplitude:</b> %{z}<br>'
+                                '<b>Distance:</b> %{marker.color}<extra></extra>',
+                                text=unique_days)])
+
+# Set layout options
+fig.update_layout(
+    scene=dict(
+        xaxis=dict(title='Magnitude'),
+        yaxis=dict(title='Shape'),
+        zaxis=dict(title='Amplitude')
+    ),
+    title='Outliers MSA Plot'
+)
+
+# Show the plot
+fig.show()
+
 
 
--- outDec.py
+++ outDec.py
@@ -36,26 +36,26 @@ def outDec(magnitude, shape, amplitude):
     shape_cdf, y_shape = empirical_cdf(shape)
     amplitude_cdf, y_amplitude = empirical_cdf(amplitude)
     
-    # Plot the empirical cdf of mag
-    fig, axes = plt.subplots(1, 1, figsize=(7, 5))
-    plt.step(magnitude_cdf, y_magnitude, label='Magnitude', axes=axes)
-    axes.legend(loc='best')
-    axes.set(xlabel='Magnitude', ylabel='p', title='Magnitude cumulative distribution function')
-    plt.show()
+    # # Plot the empirical cdf of mag
+    # fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+    # plt.step(magnitude_cdf, y_magnitude, label='Magnitude', axes=axes)
+    # axes.legend(loc='best')
+    # axes.set(xlabel='Magnitude', ylabel='p', title='Magnitude cumulative distribution function')
+    # plt.show()
     
-    # Plot the empirial cdf of shape
-    fig, axes = plt.subplots(1, 1, figsize=(7, 5))
-    plt.step(shape_cdf, y_shape, label='Shape', axes=axes)
-    axes.legend(loc='best')
-    axes.set(xlabel='Shape', ylabel='p', title='Shape cumulative distribution function')
-    plt.show()
+    # # Plot the empirial cdf of shape
+    # fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+    # plt.step(shape_cdf, y_shape, label='Shape', axes=axes)
+    # axes.legend(loc='best')
+    # axes.set(xlabel='Shape', ylabel='p', title='Shape cumulative distribution function')
+    # plt.show()
     
-    # Plot the empirial cdf of shape
-    fig, axes = plt.subplots(1, 1, figsize=(7, 5))
-    plt.step(amplitude_cdf, y_amplitude, label='Amplitude', axes=axes)
-    axes.legend(loc='best')
-    axes.set(xlabel='Amplitude', ylabel='p', title='Amplitude cumulative distribution function')
-    plt.show()
+    # # Plot the empirial cdf of shape
+    # fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+    # plt.step(amplitude_cdf, y_amplitude, label='Amplitude', axes=axes)
+    # axes.legend(loc='best')
+    # axes.set(xlabel='Amplitude', ylabel='p', title='Amplitude cumulative distribution function')
+    # plt.show()
 
     # Convert arrays to list so I can use pop() and sort them
     magnitude, shape, amplitude = list(sorted(magnitude)), list(sorted(shape)), list(sorted(amplitude))
@@ -63,59 +63,76 @@ def outDec(magnitude, shape, amplitude):
     # Get the distance between the extreme points in mag and in shape
     distance_magnitude, distance_shape, distance_amplitude = abs(magnitude[-1] - magnitude[0]), abs(shape[-1] - shape[0]), abs(amplitude[-1] - amplitude[0])
     
-    # # Get mag crushing data
-    # mag_crushed = []
-    # for i in range(len(mag)-1):
-    #     if is_even(i) == True:
-    #         mag.pop(-1)
-    #         distance_mag_updated = abs(mag[-1] - mag[0])
+    # Get mag crushing data
+    magnitude_crushed = []
+    for i in range(len(magnitude)-1):
+        if is_even(i) == True:
+            magnitude.pop(-1)
+            distance_magnitude_updated = abs(magnitude[-1] - magnitude[0])
 
-    #     elif is_even(i) == False:
-    #         mag.pop(0)
-    #         distance_mag_updated = abs(mag[-1] - mag[0])
+        elif is_even(i) == False:
+            magnitude.pop(0)
+            distance_magnitude_updated = abs(magnitude[-1] - magnitude[0])
         
-    #     mag_crushed.append(np.round((distance_mag_updated/distance_mag) * 100, 3))
+        magnitude_crushed.append(np.round((distance_magnitude_updated/distance_magnitude) * 100, 3))
     
-    # # Get shape crushed data
-    # shape_crushed = []
-    # for i in range(len(shape)-1):
-    #     shape.pop(-1)
-    #     distance_shape_updated = abs(shape[-1] - shape[0])
+    # Get shape crushed data
+    shape_crushed = []
+    for i in range(len(shape)-1):
+        shape.pop(-1)
+        distance_shape_updated = abs(shape[-1] - shape[0])
         
-    #     shape_crushed.append(np.round((distance_shape_updated/distance_shape) * 100, 3))
+        shape_crushed.append(np.round((distance_shape_updated/distance_shape) * 100, 3))
     
-    # # Plot the crushed mag and shape
-    # plt.plot(mag_crushed, np.arange(0, len(mag_crushed)), label = 'mag')
+    # Get amplitude crushed data
+    amplitude_crushed = []
+    for i in range(len(amplitude)-1):
+        amplitude.pop(-1)
+        distance_amplitude_updated = abs(amplitude[-1] - amplitude[0])
+        
+        amplitude_crushed.append(np.round((distance_amplitude_updated/distance_amplitude) * 100, 3))
+    
+    # Plot the crushed mag and shape
+    # plt.plot(magnitude_crushed, np.arange(0, len(magnitude_crushed)), label = 'magnitude')
     # plt.plot(shape_crushed, np.arange(0, len(shape_crushed)), label = 'shape')
+    # plt.plot(amplitude_crushed, np.arange(0, len(amplitude_crushed)), label = 'amplitude')
     # plt.legend(loc='best')
     # plt.show()
 
-    # # Get the point where the elbow/knee starts flattening
-    # kl_mag = KneeLocator(mag_crushed, np.arange(0, len(mag_crushed)), curve='convex', direction='decreasing')
-    # kl_mag_point = kl_mag.knee
-    # # kl_mag.plot_knee()
+    # Get the point where the elbow/knee starts flattening
+    kl_magnitude = KneeLocator(magnitude_crushed, np.arange(0, len(magnitude_crushed)), curve='convex', direction='decreasing')
+    kl_magnitude_point = kl_magnitude.knee
+    # kl_magnitude.plot_knee()
+    # kl_mag.plot_knee()
 
-    # kl_shape = KneeLocator(shape_crushed, np.arange(0, len(shape_crushed)), curve='convex', direction='decreasing')
-    # kl_shape_point = kl_shape.knee
+    kl_shape = KneeLocator(shape_crushed, np.arange(0, len(shape_crushed)), curve='convex', direction='decreasing')
+    kl_shape_point = kl_shape.knee
     # kl_shape.plot_knee()
     # plt.show()
-
-    # print('Mag knee:', kl_mag_point, 'Shape knee:', kl_shape_point)
-
-    # # Define wether there are outliers or not in the data based on the value of the knee/elbow points
-    # if (kl_mag_point is None) and (kl_shape_point is None):
-    #     outliers_in_data = False
-    # else:
-    #     if (kl_mag_point is None):
-    #         kl_mag_point = 100
-    #     if (kl_shape_point is None):
-    #         kl_shape_point = 100
-    #     if (kl_mag_point <= 15) or (kl_shape_point <= 15):
-    #         outliers_in_data = True
-    #     else:
-    #         outliers_in_data = False
     
-    # return outliers_in_data
+    kl_amplitude = KneeLocator(amplitude_crushed, np.arange(0, len(amplitude_crushed)), curve='convex', direction='decreasing')
+    kl_amplitude_point = kl_amplitude.knee
+    # kl_amplitude.plot_knee()
+    # plt.show()
+
+    print('Magnitude knee:', kl_magnitude_point, 'Shape knee:', kl_shape_point, 'Amplitude knee:', kl_amplitude_point)
+
+    # Define wether there are outliers or not in the data based on the value of the knee/elbow points
+    if (kl_magnitude_point is None) and (kl_shape_point is None) and (kl_amplitude_point is None):
+        outliers_in_data = False
+    else:
+        if (kl_magnitude_point is None):
+            kl_magnitude_point = 100
+        if (kl_shape_point is None):
+            kl_shape_point = 100
+        if (kl_amplitude_point is None):
+            kl_amplitude_point = 100
+        if (kl_magnitude_point <= 15) or (kl_shape_point <= 15) or (kl_amplitude_point <= 15):
+            outliers_in_data = True
+        else:
+            outliers_in_data = False
+    
+    return outliers_in_data
 
 def outDec2_testing(varName, clean):
     
@@ -134,41 +151,49 @@ def outDec2_testing(varName, clean):
         shape = list(filter(lambda x: x <= (np.percentile(shape, 50)), shape))
     
     # Get the empirial cdf of mag and shape
-    # mag_cdf, y_mag = empirical_cdf(mag)
-    # shape_cdf, y_shape = empirical_cdf(shape)
+    magnitude_cdf, y_magnitude = empirical_cdf(magnitude)
+    shape_cdf, y_shape = empirical_cdf(shape)
+    amplitude_cdf, y_amplitude = empirical_cdf(amplitude)
     
-    # # Plot the empirical cdf of mag
-    # fig, axes = plt.subplots(1, 1, figsize=(7, 5))
-    # plt.step(mag_cdf, y_mag, label='Magnitude', axes=axes)
-    # axes.legend(loc='best')
-    # axes.set(xlabel='Magnitude', ylabel='p', title='Magnitude cumulative distribution function')
+    # Plot the empirical cdf of mag
+    fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+    plt.step(magnitude_cdf, y_magnitude, label='Magnitude', axes=axes)
+    axes.legend(loc='best')
+    axes.set(xlabel='Magnitude', ylabel='p', title='Magnitude cumulative distribution function')
     # plt.show()
     
-    # # Plot the empirial cdf of shape
-    # fig, axes = plt.subplots(1, 1, figsize=(7, 5))
-    # plt.step(shape_cdf, y_shape, label='Shape', axes=axes)
-    # axes.legend(loc='best')
-    # axes.set(xlabel='Shape', ylabel='p', title='Shape cumulative distribution function')
+    # Plot the empirial cdf of shape
+    fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+    plt.step(shape_cdf, y_shape, label='Shape', axes=axes)
+    axes.legend(loc='best')
+    axes.set(xlabel='Shape', ylabel='p', title='Shape cumulative distribution function')
     # plt.show()
     
+    # Plot the empirial cdf of shape
+    fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+    plt.step(amplitude_cdf, y_amplitude, label='Amplitude', axes=axes)
+    axes.legend(loc='best')
+    axes.set(xlabel='Amplitude', ylabel='p', title='Amplitude cumulative distribution function')
+    # plt.show()
+
     # Convert arrays to list so I can use pop() and sort them
-    mag, shape = list(sorted(mag)), list(sorted(shape))
-    
+    magnitude, shape, amplitude = list(sorted(magnitude)), list(sorted(shape)), list(sorted(amplitude))
+
     # Get the distance between the extreme points in mag and in shape
-    distance_mag, distance_shape = abs(mag[-1] - mag[0]), abs(shape[-1] - shape[0])
+    distance_magnitude, distance_shape, distance_amplitude = abs(magnitude[-1] - magnitude[0]), abs(shape[-1] - shape[0]), abs(amplitude[-1] - amplitude[0])
     
     # Get mag crushing data
-    mag_crushed = []
-    for i in range(len(mag)-1):
+    magnitude_crushed = []
+    for i in range(len(magnitude)-1):
         if is_even(i) == True:
-            mag.pop(-1)
-            distance_mag_updated = abs(mag[-1] - mag[0])
+            magnitude.pop(-1)
+            distance_magnitude_updated = abs(magnitude[-1] - magnitude[0])
 
         elif is_even(i) == False:
-            mag.pop(0)
-            distance_mag_updated = abs(mag[-1] - mag[0])
+            magnitude.pop(0)
+            distance_magnitude_updated = abs(magnitude[-1] - magnitude[0])
         
-        mag_crushed.append(np.round((distance_mag_updated/distance_mag) * 100, 3))
+        magnitude_crushed.append(np.round((distance_magnitude_updated/distance_magnitude) * 100, 3))
     
     # Get shape crushed data
     shape_crushed = []
@@ -178,37 +203,53 @@ def outDec2_testing(varName, clean):
         
         shape_crushed.append(np.round((distance_shape_updated/distance_shape) * 100, 3))
     
+    # Get amplitude crushed data
+    amplitude_crushed = []
+    for i in range(len(amplitude)-1):
+        amplitude.pop(-1)
+        distance_amplitude_updated = abs(amplitude[-1] - amplitude[0])
+        
+        amplitude_crushed.append(np.round((distance_amplitude_updated/distance_amplitude) * 100, 3))
+    
     # Plot the crushed mag and shape
-    plt.plot(mag_crushed, np.arange(0, len(mag_crushed)), label = 'mag')
+    plt.plot(magnitude_crushed, np.arange(0, len(magnitude_crushed)), label = 'magnitude')
     plt.plot(shape_crushed, np.arange(0, len(shape_crushed)), label = 'shape')
+    plt.plot(amplitude_crushed, np.arange(0, len(amplitude_crushed)), label = 'amplitude')
     plt.legend(loc='best')
-    plt.show()
+    # plt.show()
 
     # Get the point where the elbow/knee starts flattening
-    kl_mag = KneeLocator(mag_crushed, np.arange(0, len(mag_crushed)), curve='convex', direction='decreasing')
+    kl_mag = KneeLocator(magnitude_crushed, np.arange(0, len(magnitude_crushed)), curve='convex', direction='decreasing')
     kl_mag_point = kl_mag.knee
-    kl_mag.plot_knee()
+    # kl_mag.plot_knee()
 
     kl_shape = KneeLocator(shape_crushed, np.arange(0, len(shape_crushed)), curve='convex', direction='decreasing')
     kl_shape_point = kl_shape.knee
     kl_shape.plot_knee()
-    plt.show()
+    # plt.show()
+    
+    kl_amplitude = KneeLocator(amplitude_crushed, np.arange(0, len(amplitude_crushed)), curve='convex', direction='decreasing')
+    kl_amplitude_point = kl_amplitude.knee
+    kl_amplitude.plot_knee()
+    # plt.show()
 
-    print('Mag knee:', kl_mag_point, 'Shape knee:', kl_shape_point)
+    print('Magnitude knee:', kl_mag_point, 'Shape knee:', kl_shape_point, 'Amplitude knee:', kl_amplitude_point)
 
     # Define wether there are outliers or not in the data based on the value of the knee/elbow points
-    if (kl_mag_point is None) and (kl_shape_point is None):
+    if (kl_mag_point is None) and (kl_shape_point is None) and (kl_amplitude_point is None):
         outliers_in_data = False
     else:
         if (kl_mag_point is None):
             kl_mag_point = 100
         if (kl_shape_point is None):
             kl_shape_point = 100
-        if (kl_mag_point <= 15) or (kl_shape_point <= 15):
+        if (kl_amplitude_point is None):
+            kl_amplitude_point = 100
+        if (kl_mag_point <= 15) or (kl_shape_point <= 15) or (kl_amplitude_point <= 15):
             outliers_in_data = True
         else:
             outliers_in_data = False
-        
+    
     return outliers_in_data
 
 
