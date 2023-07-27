54a87db1d5da87fdc4056670654da902000d392c
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-07-27 11:33:08 -0700

    Integrated the shuffler in the main class

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import rpy2.robjects as robjects
 import plotly.graph_objects as go
 
 from outDec import outDec
+from random import shuffle
 from datetime import datetime
 
 class MSA():
@@ -28,6 +29,33 @@ class MSA():
         self.distances = None
         self.outliers_in_data = None
         self.index_outliers = None
+    
+    def shuffler(self):
+        """his file shuffles the database by day.
+        ----------
+        Arguments:
+        self.
+        
+        Return:
+        None."""
+        
+        data = pd.read_csv(f'data/labeled_{self.station}_pro.csv', sep=',', encoding='utf-8')
+        
+        # Group the data by day
+        data['date'] = pd.to_datetime(data['date'])
+        grouped_by_day = data.groupby(data['date'].dt.date)
+
+        # Shuffle order of the groups
+        group_keys = list(grouped_by_day.groups.keys())
+        shuffle(group_keys)
+        
+        # Combine the suffle groups back into a single data frame
+        shuffled_df = pd.concat([grouped_by_day.get_group(key) for key in group_keys])
+
+        # Reset the index of the suffled data frame
+        shuffled_df.reset_index(drop=True, inplace=True)
+        
+        shuffled_df.to_csv(f'data/labeled_{station}_shu.csv', sep=',', index=False)
     
     def get_timestamps(self):
         
