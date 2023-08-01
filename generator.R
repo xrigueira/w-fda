5d2f8fa3ffa518abd790bdf7f5700e784b151ec6
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-07-31 17:27:08 -0700

    Started data generation

--- generator.R
+++ generator.R
@@ -5,3 +5,17 @@
 # https://journal.r-project.org/archive/2019/RJ-2019-032/RJ-2019-032.pdf
 
 library(roahd)
+
+data_generator <- function() {
+
+    # Define its parameters
+    N <- 100 # Number of distintc functional observations
+    L <- 6 # Number of components of the data
+    P <- 1e2
+    time_grid <- seq(0, 1, length.out = P)
+    centerline <-  matrix(c(sin(2 * pi * time_grid), sqrt(time_grid), 10 * (time_grid - 0.5) * time_grid), nrow = 3, byrow = TRUE)
+
+    # Continue here. Understand well how this works. At least its parameters
+
+    generate_gauss_mfdata(N = N, L = L, centerline = centerline)
+}
