53d5d80d61b68bfb20b8b8f57aaaa00bd62a979c
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-07-31 17:14:12 -0700

    Simulation files added

---
+++ fadalara.R
@@ -0,0 +1,6 @@
+# This file uses the methods from the "adamethods" library to implement
+# the fadalara method for multivariate data
+
+# https://cran.r-project.org/web/packages/adamethods/index.html
+
+library(adamethods)
---
+++ generator.R
@@ -0,0 +1,7 @@
+# This file uses the methods implemented in the "roahd" library
+# to generate multivariate functional data
+
+# https://cran.r-project.org/web/packages/roahd/index.html
+# https://journal.r-project.org/archive/2019/RJ-2019-032/RJ-2019-032.pdf
+
+library(roahd)
