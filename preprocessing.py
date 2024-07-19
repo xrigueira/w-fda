e85c6c81c8f6b1b065c271f289d9a2d10e8f955d
Xurxo Rigueira <xrigueiradiaz@gmail.com>
2024-07-19 17:51:02 +0200

    logger in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from preprocessors.checkGaps import checkGaps
 from preprocessors.filler import filler
@@ -6,6 +7,9 @@ from preprocessors.joiner import joiner
 from preprocessors.labeler import labeler
 from preprocessors.filterer import mfilterer
 from preprocessors.smoother import smoother
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 """Preprocessing starts from the original univariate txt files."""
 
@@ -25,29 +29,29 @@ if __name__ == '__main__':
     
         # Find the gaps in the time series, add them and leave them blank
         checkGaps(File=f'{varName}.txt', timestep=timeStep, varname=varName)
-        print(f'[INFO] checkGaps() {varName} DONE')
+        logging.info(f'checkGaps() {varName} DONE')
 
         # Add 31s if timestep is 'a' and week information. See filler.py for details
         filler(File=f'{varName}_full.csv', timeframe=timeFrame, timestep=timeStep, varname=varName)
-        print(f'[INFO] filler() {varName} DONE')
+        logging.info(f'filler() {varName} DONE')
     
-    # for station in stations:
+    for station in stations:
         
-    #         # Join the filed databases
-    #         joiner(station=station)
-    #         print(f'[INFO] joiner() {station} DONE')
+            # Join the filed databases
+            joiner(station=station)
+            logging.info(f'joiner() {station} DONE')
 
-    # # Add the Label data to all stations
-    # labeler(stations=stations)
-    # print('[INFO] labeler() DONE')
+    # Add the Label data to all stations
+    labeler(stations=stations)
+    logging.info('labeler() DONE')
     
-    # for station in stations:
+    for station in stations:
 
-        # # Filter out those months or weeks or days (depending on the desired
-        # # time unit) with too many NaN in several variables and iterate on the rest
-        # mfilterer(File=f'labeled_{station}.csv', timeframe=timeFrame, timestep=timeStep)
-        # print(f'[INFO] filterer() {station} DONE')
+        # Filter out those months or weeks or days (depending on the desired
+        # time unit) with too many NaN in several variables and iterate on the rest
+        mfilterer(File=f'labeled_{station}.csv', timeframe=timeFrame, timestep=timeStep)
+        logging.info(f'mfilterer() {station} DONE')
 
-        # # Smooth the data. THIS IS NOT DONE IN THIS REPO
-        # smoother(station=station)
-        # print(f'[INFO] smoother() {station} DONE')
+        # Smooth the data. THIS IS NOT DONE IN THIS REPO
+        smoother(station=station)
+        logging.info(f'smoother() {station} DONE')
